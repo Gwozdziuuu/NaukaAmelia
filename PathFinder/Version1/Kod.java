96f002716cb7018cbe4725fd14fa560b7a724560
Marcin <m.gwozdz@pixel.pl>
2021-03-17 10:37:41 +0100

    PathFinder
    Version 1

---
+++ PathFinder/Version1/Kod.java
@@ -0,0 +1,31 @@
+public class Test {
+
+    public static void main(String[] args) {
+        printArray(getFullHorizontalRow());
+        for (int i = 0; i < 5; i++) {
+            printArray(getMiddleHorizontalRow());
+        }
+        printArray(getFullHorizontalRow());
+    }
+
+    private static String[] getMiddleHorizontalRow() {
+        //
+        // Uzupełnić
+        //
+    }
+
+    private static String[] getFullHorizontalRow() {
+        String[] lines = new String[10];
+        //
+        // Uzupełnić
+        //
+        return lines;
+    }
+
+    private static void printArray(String[] array) {
+        //
+        // Uzupełnić
+        //
+    }
+
+}
